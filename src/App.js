487a3d7b4c53ef220bfcbe2ca28b8536cf1dd4c3
Gabriel <gabesenacom@gmail.com>
2021-08-20 22:08:32 -0300

    Add App.js and App.css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,108 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import uniqid from 'uniqid'
+import TaskList from './components/TaskList'
+import CompletedTaskList from './components/CompletedTaskList'
+import TaskManager from './components/TaskManager'
+import Footer from './components/Footer'
+import './styles/App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+
+    this.state = {
+      tasks: [],
+      task: {
+        text: '',
+        id: uniqid()
+      }
+    }
+  }
+
+  handleInputChange = event => {
+    this.setState(prevState => ({
+      task: { text: event.target.value, id: prevState.task.id }
+    }))
+  }
+
+  handleSubmitTask = event => {
+    if (this.state.task.text.length === 0) return
+    event.preventDefault()
+    event.target.reset()
+    this.addTask()
+  }
+
+  removeTask = id => {
+    this.setState(prevState => ({
+      tasks: prevState.tasks.filter(task => task.id !== id)
+    }))
+  }
+
+  addTask = () => {
+    this.setState(prevState => ({
+      tasks: prevState.tasks.concat(prevState.task),
+      task: {
+        text: '',
+        id: uniqid()
+      }
+    }))
+  }
+
+  defineTaskEditor = (id, isEditor) => {
+    this.setState(prevState => ({
+      tasks: prevState.tasks.map(task => {
+        if (task.id === id) {
+          task.editor = isEditor
+        }
+        return task
+      })
+    }))
+  }
+
+  changeTaskText = (id, newText) => {
+    this.setState(prevState => ({
+      tasks: prevState.tasks.map(task => {
+        if (task.id === id) {
+          task.text = newText
+          task.editor = false
+        }
+        return task
+      })
+    }))
+  }
+
+  checkTask = (id) => {
+    this.setState(prevState => ({
+      tasks: prevState.tasks.map(task => {
+        if (task.id === id) {
+          task.completed = true
+        }
+        return task
+      })
+    }))
+  }
+
+  render () {
+    return (
+        <main>
+        <h1 className='title'>Task App</h1>
+        <TaskManager
+          submitTaskHandler={this.handleSubmitTask}
+          inputChangeHandler={this.handleInputChange}
+        />
+        <TaskList
+          tasks={this.state.tasks}  
+          onRemoveTask={this.removeTask}
+          defineTaskEditor={this.defineTaskEditor}
+          onChangeTaskText={this.changeTaskText}
+          onCompleteTask={this.checkTask}
+        />
+        <CompletedTaskList
+          tasks={this.state.tasks}
+        />
+        <Footer/>
+      </main>
+      
+    )
+  }
 }
-
-export default App;
